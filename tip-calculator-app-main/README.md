44baf8b89313666071ce7565994550c86e4fdc83
CMarghin <developermarghin@gmail.com>
2021-07-14 13:39:54 +0200

    add tip calculator project

---
+++ tip-calculator-app-main/README.md
@@ -0,0 +1,60 @@
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [Continued development](#continued-development)
+- [Me](#Me)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+### Screenshot
+
+![](./images/Screenshot.png)
+![](./images/Screenshot-phone.png)
+
+### Links
+- challenge URL: [Frontend mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX)
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- pure CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- Vanilla Js (practicing)
+
+
+### Continued development
+
+I think I need to learn more about layouts, maybe learn grid systeme
+***everything is flexbox***, that what I wanted to avoid during the process of this project but I think I failed.
+I need to develope my DOM skill, actually I didn't learn it I just Use the help of Emmet on Vscode and trying to make some sense .
+I have to learn a framwork or a library. maybe!
+
+
+
+## Me
+
+- Frontend Mentor - [@CMarghin](https://www.frontendmentor.io/profile/CH4R4F)
+- Twitter - [@CMarghin](https://www.twitter.com/CMarghin)
---
+++ tip-calculator-app-main/assets/index.js
@@ -0,0 +1,68 @@
+"use strict";
+let tipAmount = document.querySelector(".tip-result"),
+    total = document.querySelector(".total-result"),
+    bill = document.querySelector("#bill-input"),
+    nop = document.querySelector("#nop-input"),
+    tip = document.querySelectorAll(".tip-value"),
+    reset = document.querySelector(".reset-results"),
+    checkedTip = [false],
+    tipValue = null;
+
+//check the tip value and listen to the buttons
+tip.forEach(button => {
+
+    // check if the cklicked option is a button or the custom input 
+    function selectTip() {
+
+        if(checkedTip[0]) {
+            checkedTip[1].classList.remove("active")
+            button.classList.add("active")
+            checkedTip[1] = button
+        } else {
+            checkedTip[0] = true
+            checkedTip[1] = button
+            checkedTip[1].classList.add("active")
+        }
+
+        tipValue = button.value
+    }
+
+    if(button.nodeName == "BUTTON") {
+        button.addEventListener("click", selectTip)
+        button.addEventListener("click", checkInfos)
+
+    } else {
+        button.addEventListener("focus", selectTip)
+        button.addEventListener("input", selectTip)
+        button.addEventListener("input", checkInfos)
+    }
+})
+
+function initResults() {
+    tipAmount.textContent = "0.00"
+    total.textContent = "0.00"
+}
+function checkInfos() {
+
+    let tipAmountValue = eval((bill.value * (tipValue / 100)) / nop.value),
+        totalValue = eval(bill.value / nop.value + tipAmountValue);
+
+    if(bill.value && nop.value && tipValue) {
+        tipAmount.textContent = tipAmountValue.toFixed(2)
+        total.textContent = totalValue.toFixed(2)
+    } else {
+        initResults()
+    }
+}
+
+bill.addEventListener("input", checkInfos)
+nop.addEventListener("input", checkInfos)
+
+reset.addEventListener("click", function(){
+    bill.value = ""
+    nop.value = ""
+    tipValue = null
+    checkedTip[0] = false
+    checkedTip[1].classList.remove("active")
+    initResults()
+})
